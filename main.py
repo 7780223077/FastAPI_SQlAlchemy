5a8d95594492a9d580f2fa4c3556050dca344a63
charan reddy <gundacharanreddy7@gmail.com>
2024-11-20 22:55:05 +0530

    fastapi curd operations using sqlalchemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-from typing import Annotated, List
+from typing import Annotated, Optional
 from database import SessionLocal, create_db
 import models
 from sqlalchemy.orm import Session
@@ -13,8 +13,8 @@ create_db()
 class EmployeeBase(BaseModel):
     first_name: str
     last_name: str
-    gender: str
-    age: int
+    gender: Optional[str] =None
+    age: Optional[str] = None
 
 def get_db():
     db = SessionLocal()
@@ -33,7 +33,30 @@ async def register_user(emp: EmployeeBase, db: db_dependency):
     db.refresh(db_emp)
     return {"id": db_emp.id, "first_name": db_emp.first_name, "last_name": db_emp.last_name}
 
-@app.get("/api/mployee/get")
+
+@app.get("/api/employee/get")
 async def get_all_users(db:db_dependency):
     employees = db.query(models.Employee).all()
     return employees
+
+@app.delete("/api/employee/{emp_id}")
+async def delete_employee(emp_id: int, db: db_dependency):
+    db_emp = db.query(models.Employee).filter(models.Employee.id == emp_id).first()
+    if db_emp is None:
+        raise HTTPException(status_code=404, detail=f"employee not found with id: '{emp_id}'")
+    db.delete(db_emp)
+    db.commit()
+    return {"detail": "Employee deleted successfully"}
+
+@app.put("/api/employee/update")
+async def update_employee(emp_id:int, emp : EmployeeBase, db: db_dependency):
+    db_emp = db.query(models.Employee).filter(models.Employee.id == emp_id).first()
+    if db_emp is None:
+        raise HTTPException(status_code=404, detail=f"employee not found with id: '{emp_id}'")
+    db_emp.first_name = emp.first_name
+    db_emp.last_name = emp.last_name
+    db_emp.gender = emp.gender
+    db_emp.age = emp.age
+    db.commit()
+    db.refresh(db_emp)
+    return db_emp
